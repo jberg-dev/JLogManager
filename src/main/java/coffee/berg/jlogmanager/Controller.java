3b73fa46dd5db0214d4b413c3762b72e91973a2f
Bergerking <joel.amundberg@gmail.com>
2018-10-14 18:31:09 +0200

    Bare bones

---
+++ src/main/java/coffee/berg/jlogmanager/Controller.java
@@ -0,0 +1,15 @@
+package coffee.berg.jlogmanager;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by Bergerking on 2018-10-14.
+ */
+public class Controller
+{
+	@FXML
+	public void Exit()
+	{
+		System.exit(1);
+	}
+}
